a23c863d4151d35f20b2322f48c98576a7da2dbf
Vivek Mangla <vivekFuneesh@users.noreply.github.com>
2014-10-16 00:18:23 +0530

    Create AboutPanel.java

---
+++ AboutPanel.java
@@ -0,0 +1,26 @@
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author vivek
+ */
+public class AboutPanel extends JPanel {
+    
+   
+   public AboutPanel(){
+    
+       
+       
+   }
+    
+    
+    @Override
+    public void paintComponent(Graphics g){super.paintComponent(g);
+    
+    
+   
+    }
+    
+}
